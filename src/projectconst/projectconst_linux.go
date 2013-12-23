825467aa4aea0776b881ce869487f3b72a0f834b
Yanghe Liu <liuyanghejerry@126.com>
2013-12-23 10:49:53 +0800

    update: move files into correct dir

--- src/projectconst/projectconst_linux.go
+++ src/projectconst/projectconst_linux.go
@@ -5,6 +5,8 @@ package projectconst
 const (
 	SERVER_ADDR_IPV4           = "http://update.mrspaint.com:17979"
 	SERVER_ADDR_IPV6           = "http://update6.mrspaint.com:17979"
+	SERVER_HOST_IPV4           = "update.mrspaint.com"
+	SERVER_HOST_IPV6           = "update6.mrspaint.com"
 	DEFAULT_NEW_PACKAGE        = ""
 	UPDATER_VER         uint64 = 20
 )
