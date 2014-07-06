fa43d0ebbb05df282ea64144690e716232363cad
liuyanghejerry <liuyanghejerry@126.com>
2014-07-06 17:09:58 +0800

    修正safekill的功能

--- src/safekill/safekill.go
+++ src/safekill/safekill.go
@@ -6,10 +6,11 @@ package safekill
 // import "os"
 import "syscall"
 
-// send SIGINT if on unix-like system
+// send SIGUSR2 if on unix-like system
+var HandleSignal = syscall.SIGUSR2
 
 func SafeKill(pid int) error {
 	// SIGINT is caught by both parent process and child process
 	// but child process will handle it so that installation can go on
-	return syscall.Kill(pid, syscall.SIGINT)
+	return syscall.Kill(pid, HandleSignal)
 }
